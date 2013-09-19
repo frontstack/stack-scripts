adbdd4cf3f00fab912a30dbb6c4b1d5b3e702fe7
Tomas <tomas@aparicio.me>
2013-09-20 00:46:46 +0200

    Added build script

---
+++ build-release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ENV_PATH="$(echo `dirname $0`)/../frontstack-env-x64" #`dirname $(readlink -f "$0")`"/../frontstack-env-x64"
+OUTPUT_PATH="../releases" #`dirname $(readlink -f "$0")`"/../releases/"
+
+read -p "Generate new release? (y/N): " input
+
+case $input in y|Y|yes|YES|Yes)
+  echo " "
+;;
+*)
+  echo "Exiting."
+  exit 0
+;;
+esac
+
+read -p "Enter the version number? (e.g: 1.0.0): " version
+
+if [ -z $version ]; then
+  echo "You must enter the release version number. Exiting"
+  exit 0
+fi
+
+if [ ! -d $OUTPUT_PATH ]; then
+	mkdir $OUTPUT_PATH
+fi
+
+echo "Generating the release file in: $OUTPUT_PATH"
+sleep 1
+
+cd "$ENV_PATH"
+
+tar cvzf "$OUTPUT_PATH/frontstack-$version-x64.tar.gz" * \
+  --exclude .git \
+  --exclude *.a 
